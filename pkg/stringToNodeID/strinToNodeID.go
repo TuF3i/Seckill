b23e772d4b83580ab0610da85aa9e668964694af
TuF3i <13759301652@163.com>
2026-05-05 17:21:46 +0800

    🐛 修复NodeID为负数的问题

--- pkg/stringToNodeID/strinToNodeID.go
+++ pkg/stringToNodeID/strinToNodeID.go
@@ -5,5 +5,10 @@ import "hash/fnv"
 func StringToNodeID(s string) int64 {
 	h := fnv.New64a()
 	h.Write([]byte(s))
-	return int64(h.Sum64()) % 1023
+
+	val := int64(h.Sum64())
+	if val < 0 {
+		val = -val
+	}
+	return val % 1024
 }
